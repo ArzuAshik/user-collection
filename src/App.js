bff9212235b285d662060800dec1ed59a895861c
ArzuAshik <arzu2050@gmail.com>
2020-08-26 13:05:46 +0600

    for map and setUser  problem review

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function App() {
+
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => createUser(data))
+
+    
+    console.log('loging from Effect', user);
+  }, [])
+
+  function createUser(data) {
+    // let allUser = [];
+    data.map(singleUser => {
+      fetch('https://dog.ceo/api/breeds/image/random')
+        .then(res => res.json())
+        .then(img => {
+          const singleInfo = {
+            id: singleUser.id,
+            name: singleUser.name,
+            email: singleUser.email,
+            img: img.message,
+          }
+          // allUser = [...allUser, singleInfo];
+          // setUser(allUser);
+          const newUser = [...user, singleInfo]
+          setUser(newUser);
+          
+          console.log(user);
+        })
+    })
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Total user: {user.length}</h1>
+      {
+        console.log('final', user)
+      }
     </div>
   );
 }
 
 export default App;
+
